87fbeb08521c6455fa020e438e4e3c806dd0ecef
Space <75291619+Fateixn@users.noreply.github.com>
2022-04-25 02:06:31 +0530

    Add files via upload

---
+++ Login to your account/temp.js
@@ -0,0 +1,16 @@
+function validate()
+{
+var username=document.getElementById("input-email").value;
+var password=document.getElementById("input-password").value;
+if(username=="CosmicDuke" && password=='Yash@2004')
+{
+    window.open("https://fateixn.github.io/fatespace/",);
+    
+}
+else
+{
+    alert("Wrong Password");
+
+}
+
+}
